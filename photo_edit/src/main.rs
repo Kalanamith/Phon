4771028fe18e31982d0f4555be4c0f2ad5242fd0
kalanamith <kalanamith@gmail.com>
2020-07-15 19:01:00 +1200

    Fix user input

--- photo_edit/src/main.rs
+++ photo_edit/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("Photo orientation fixer version 0.0.1");
 
     // TODO: move this to separate function
+    // Capture panic here and log a message
     read_user_input()
 }
 
@@ -15,6 +16,7 @@ fn read_user_input() {
     let mut file_names: Vec<String> = env::args().collect();
 
     if file_names.len() == 0 {
+        // TODO: Panic here!
         println!("No file names were given");
     } else {
         file_names.remove(0);
