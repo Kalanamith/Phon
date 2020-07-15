1cacdc974060978dfa7930985668c5c79364e9ef
kalanamith <kalanamith@gmail.com>
2020-07-15 21:01:52 +1200

    Removed compiler warnings

--- photo_edit/src/main.rs
+++ photo_edit/src/main.rs
@@ -7,6 +7,10 @@ fn main() {
 
     // TODO: move this to separate function
     // Capture panic here and log a message
+    // match read_user_input() {
+    //     Err(err) => println!("Error processing images {}", err),
+    //     _ => println!("Processing")
+    // }
     read_user_input()
 }
 
@@ -14,13 +18,14 @@ fn main() {
 /// Calls method `exif_fixer()`
 fn read_user_input() {
     let mut file_names: Vec<String> = env::args().collect();
-
+    file_names.remove(0);
     if file_names.len() == 0 {
         // TODO: Panic here!
         println!("No file names were given");
-    } else {
-        file_names.remove(0);
     }
+    // else {
+    //
+    // }
 
     match exif_reader(file_names) {
         Err(err) => println!("{:?}", err),
