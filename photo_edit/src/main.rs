1126b776ea1f6c50ef5fbb9c257c06883c9d859e
kalanamith <kalanamith@gmail.com>
2020-07-15 18:26:33 +1200

    Added dummy method to read data with a smile

--- photo_edit/src/main.rs
+++ photo_edit/src/main.rs
@@ -1,3 +1,4 @@
+use exif_fixer::exif_fixer;
 use std::env;
 
 fn main() {
@@ -5,6 +6,12 @@ fn main() {
     println!("Photo orientation fixer version 0.0.1");
 
     // TODO: move this to separate function
+    read_user_input()
+}
+
+/// Reads user input
+/// Calls method `exif_fixer()`
+fn read_user_input() {
     let mut file_name_args: Vec<String> = env::args().collect();
     file_name_args.remove(0);
     if file_name_args.len() == 0 {
@@ -12,5 +19,8 @@ fn main() {
     } else {
         file_name_args.remove(0);
     }
+
+
+    exif_fixer();
 
 }
