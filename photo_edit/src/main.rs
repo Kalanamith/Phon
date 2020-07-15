175835d8119d2b79d1b679bccc84f656e1b1bf9a
kalanamith <kalanamith@gmail.com>
2020-07-15 18:15:01 +1200

    Added user input reader

--- photo_edit/src/main.rs
+++ photo_edit/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: read version from .env file
+    println!("Photo orientation fixer version 0.0.1");
+
+    // TODO: move this to separate function
+    let mut file_name_args: Vec<String> = env::args().collect();
+    file_name_args.remove(0);
+    if file_name_args.len() == 0 {
+        println!("No file names were given");
+    } else {
+        file_name_args.remove(0);
+    }
+
 }
